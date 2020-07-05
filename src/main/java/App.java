ca0e34c12a933e08c598e9ae65bcf889952226fa
yarl79 <yarl79@yandex.ru>
2020-07-05 22:07:14 +0300

    Bean cat added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        bean2.setMessage("Another helloWorld");
+        System.out.println(bean1.getMessage());
+        System.out.println(bean2.getMessage());
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        catBean2.setMessage("Meauu twice");
+        System.out.println(catBean1.getMessage());
+        System.out.println(catBean2.getMessage());
+
+        System.out.println(bean1 == bean2);
+        System.out.println(catBean1 == catBean2);
     }
 }
